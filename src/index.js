9ce16583cd54fa84a005d09f9c5216495352ac7a
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-31 18:26:02 +0200

    Moving background image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./style.css";
 console.log("I like pizza");
+
+const bgImage = document.querySelector(".bg-image");
+const bgTravel = 5;
+document.addEventListener("mousemove", e => {
+    const x = (e.clientX / window.innerWidth * 2 - 1) * bgTravel;
+    const y = (e.clientY / window.innerHeight * 2 - 1) * bgTravel;
+    const positionX = 50 + x;
+    const positionY = 50 + y;
+    bgImage.style["background-position"] = `${positionX}% ${positionY}%`;
+});
