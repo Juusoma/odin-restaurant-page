8420e88e3469e708415209652cf877e15fcbb91c
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-31 19:00:52 +0200

    Add header logo and nav buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const bgImage = document.querySelector(".bg-image");
-const bgTravel = 5;
+const bgTravel = 2;
 document.addEventListener("mousemove", e => {
     const x = (e.clientX / window.innerWidth * 2 - 1) * bgTravel;
     const y = (e.clientY / window.innerHeight * 2 - 1) * bgTravel;
