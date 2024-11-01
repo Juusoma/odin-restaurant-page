c3a019150ebdc9ecd4735eed7e5a666d7953070f
Juuso Määttä <juusomaatta@hotmail.fi>
2024-11-01 15:31:10 +0200

    Adjust font properties

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const bgImage = document.querySelector(".bg-image");
 const bgTravel = 10;
 document.addEventListener("mousemove", e => {
-    const x = (e.clientX / window.innerWidth) * bgTravel;
-    const y = (e.clientY / window.innerHeight) * bgTravel;
+    const x = -(e.clientX / window.innerWidth) * bgTravel;
+    const y = -(e.clientY / window.innerHeight) * bgTravel;
     bgImage.style["background-position"] = `${x}px ${y}px`;
 });
