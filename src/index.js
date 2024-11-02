5210fdc876ec896c2e958c5e929ea3c061867382
Juuso Määttä <juusomaatta@hotmail.fi>
2024-11-02 18:07:22 +0200

    Change between home and menu tabs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import { buildHome } from "./home-tab.js";
+import { buildMenu } from "./menu-tab.js";
 
 const bgImage = document.querySelector(".bg-image");
 const bgTravel = 10;
@@ -9,7 +10,28 @@ document.addEventListener("mousemove", e => {
     bgImage.style["background-position"] = `${x}px ${y}px`;
 });
 
+const tabs = (function(){
+    const contentContainer = document.querySelector("#content");
+    let currentTabButton;
+    
+    function changeTab(button){
+        if(currentTabButton){
+            currentTabButton.classList.remove("active");
+        }
+        contentContainer.innerHTML = button.dataset.builder;
+        button.classList.add("active");
+        currentTabButton = button;
+    }
 
+    return {changeTab};
+}());
 
-const contentContainer = document.querySelector("#content");
-//contentContainer.innerHTML = buildHome();
+const homeButton = document.querySelector("#home-button");
+homeButton.dataset.builder = buildHome();
+const menuButton = document.querySelector("#menu-button");
+menuButton.dataset.builder = buildMenu();
+
+homeButton.addEventListener("click", () => tabs.changeTab(homeButton));
+menuButton.addEventListener("click", () => tabs.changeTab(menuButton));
+
+homeButton.dispatchEvent(new MouseEvent("click"));
---
+++ src/menu-tab.js
@@ -0,0 +1,124 @@
+function buildMenu(){
+    return `
+        <div class="menu-container">
+            <h2 class="menu-container-title">Menu</h2>
+            <div class="menu-items">
+                <div class="menu-items-section">
+                    <div class="menu-section-title">
+                        Pizza
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                </div>
+                <div class="menu-items-section">
+                    <div class="menu-section-title">
+                        Salad
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                </div>
+                <div class="menu-items-section">
+                    <div class="menu-section-title">
+                        Salad
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                </div>
+                
+                <div class="menu-items-section">
+                    <div class="menu-section-title">
+                        Drinks
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-item-info">
+                            <h4 class="menu-item-title">Crosconne</h4>
+                            <p class="menu-item-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <span class="menu-item-price">$3</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+export { buildMenu }
