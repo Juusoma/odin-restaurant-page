feaf69ec1bdaf06b897509e2d30be52d860e1d45
Juuso Määttä <juusomaatta@hotmail.fi>
2024-11-03 16:47:06 +0200

    Change contact tab headings

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -5,7 +5,7 @@ function buildContact(container){
     `
     <div class="contact-container">
         <div class="contact-information">
-            <h2>Contact Information</h2>
+            <h2>Find Us</h2>
             <ul>
                 <li>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
@@ -42,7 +42,7 @@ function buildContact(container){
             </ul>
         </div>
         <div class="contact-form">
-            <h2>Contact Us</h2>
+            <h2>Get In Touch</h2>
             <form action="">
                 <div>
                     <label for="name">Name: </label>
